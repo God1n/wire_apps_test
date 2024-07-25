59933dbe3856a02f6488676f3ad4c502307d100e
God1n <idpm.ru@gmail.com>
2024-07-26 00:13:11 +0530

    init one

---
+++ WireAppsTestNCLI/tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./App.{js,jsx,ts,tsx}", "./<custom-folder>/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: "#3490dc",
+      secondary: "#ffed4a",
+      danger: "#e3342f",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+  },
+  plugins: [],
+}
+
